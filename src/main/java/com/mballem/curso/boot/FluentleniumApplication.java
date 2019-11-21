105381a8e836299326ca2b87d28150ea8108a6b1
Cristina Vilas-Bôas <Cristina Vilas-Bas@172.16.7.67>
2019-11-21 08:08:49 -0300

    Classe de teste

---
+++ src/main/java/com/mballem/curso/boot/FluentleniumApplication.java
@@ -0,0 +1,12 @@
+package com.mballem.curso.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluentleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluentleniumApplication.class, args);
+	}
+}
