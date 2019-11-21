7bd6550d6bb5a97725c9314a369a4f9bd7b62d5c
Cristina Vilas-Bôas <Cristina Vilas-Bas@172.16.7.67>
2019-11-21 08:11:26 -0300

    paginaDepartamento

---
+++ src/test/java/com/mballem/curso/boot/paginaDepartamento.java
@@ -0,0 +1,14 @@
+package com.mballem.curso.boot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class paginaDepartamento {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
